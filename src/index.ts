324c1ad975cfac4f9f9cf38c1806ef994bdeb316
Nathanaël Derousseaux <n.derousseaux@icloud.com>
2023-03-16 19:14:19 +0100

    FEAT: fin

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-// here you put all the js you want.
+// Si menu-btn est cliqué alors on ajoute la classe "open" à la div "menu"
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector("nav");
+menuBtn.addEventListener("click", () => {
+	menu.classList.toggle("open");
+});
